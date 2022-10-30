6020a6b53931afe45babdc8771bdcbd332db9434
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-30 15:16:46 +0500

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+            
+        int mul = numberToPrint;
+        for (int i = 1; i <= power; i++) {
+            numberToPrint *= mul;
+        }
+        System.out.println(numberToPrint / mul);
     }
 
     public static void main(String[] args) {
